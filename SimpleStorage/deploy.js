ee0b659586a6b2a4730f819f335f649a0a36b48b
OctaviWSL <octavi.work@gmail.com>
2022-07-10 18:10:34 +0200

    ethers SimpleStorage

--- SimpleStorage/deploy.js
+++ SimpleStorage/deploy.js
@@ -1,8 +1,30 @@
-async function main() {}
+require("dotenv").config()
+const ethers = require("ethers")
+const fs = require("fs-extra")
+const { ConstructorFragment } = require("ethers/lib/utils")
+
+async function main() {
+    // http://127.0.0.1:7545
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+    const abi = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.abi",
+        "utf-8"
+    )
+    const binary = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.bin",
+        "utf-8"
+    )
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+    console.log("Deploying, please wait...")
+    const contract = await contractFactory.deploy()
+    await contract.deployTransaction.wait(1)
+    console.log(`Contract Address: ${contract.address}`)
+}
 
 main()
-  .then(() => ProcessingInstruction.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
